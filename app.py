760c027d361e994e22871e5c35022ee7778e311f
nkne <nicokne@gmail.com>
2025-09-30 03:32:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from supabase import create_client, Client
 
 # Datos de tu proyecto Supabase
-url = "https://clientes.supabase.co"
+url = "https://abidwxvmyvxgntkmceaj.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiaWR3eHZteXZ4Z250a21jZWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMDcyNjAsImV4cCI6MjA3NDc4MzI2MH0.IPChPgIsnMaassJpw0kaUJu3nZA8qQb3msD-rw1BoTk"
 supabase: Client = create_client(url, key)
 

436275a0095155460303dac11c98bd2f7f89fd02
nkne <nicokne@gmail.com>
2025-09-30 03:18:40 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from supabase import create_client, Client
+
+# Datos de tu proyecto Supabase
+url = "https://clientes.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiaWR3eHZteXZ4Z250a21jZWFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyMDcyNjAsImV4cCI6MjA3NDc4MzI2MH0.IPChPgIsnMaassJpw0kaUJu3nZA8qQb3msD-rw1BoTk"
+supabase: Client = create_client(url, key)
+
+st.title("Tabla de Clientes")
+
+if st.button("Mostrar todos los clientes"):
+    # Traer datos de Supabase
+    response = supabase.table("clientes").select("*").execute()
+    
+    if response.data:
+        # Mostrar tabla completa
+        st.table(response.data)
+    else:
+        st.write("No hay clientes en la base de datos.")
